c33023fc670724bf4b539d38bc54b85a39b9830a
Helcio da Silva <helciodasilva@gmail.com>
2018-09-15 21:19:13 -0300

    Capítulo 5

--- site-institucional/site-institucional/application/config/hooks.php
+++ site-institucional/site-institucional/application/config/hooks.php
@@ -1,6 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$hook['display_override'][] = array(
+	'class' => '',
+	'function' => 'compress',
+	'filename' => 'compress.php',
+	'filepath' => 'hooks'
+);
 /*
 | -------------------------------------------------------------------------
 | Hooks
---
+++ site-institucional/site-institucional/application/controllers/Institucional.php
@@ -0,0 +1,30 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Institucional extends CI_Controller {
+	public function index()
+	{
+		$data['title'] = "LCI | Home";
+		$data['description'] = "Exercício de exemplo do capítulo 5 do livro CodeIgniter";
+		$this->load->view('home', $data);
+	}
+
+	public function Empresa()
+	{
+		$data['title'] = "LCI | A Empresa";
+		$data['description'] = "Informações sobre a empresa";
+		$this->load->view('commons/header',$data);
+		$this->load->view('empresa');
+		$this->load->view('commons/footer');
+	}
+	
+	public function Servicos()
+	{
+		$data['title'] = "LCI | Serviços";
+		$data['description'] = "Informações sobre os serviços prestados";
+		$this->load->view('servicos',$data);
+	}
+	
+}
+?>
