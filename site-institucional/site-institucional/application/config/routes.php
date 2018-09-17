31c28d5af76b17aba9966bdd00674f585773d7de
Helcio da Silva <helciodasilva@gmail.com>
2018-09-17 07:33:41 -0300

    Capítulo 6

--- site-institucional/site-institucional/application/config/routes.php
+++ site-institucional/site-institucional/application/config/routes.php
@@ -54,3 +54,5 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['empresa']	=	"Institucional/Empresa";
 $route['servicos']	=	"Institucional/Servicos";
+$route['fale-conosco'] = "Contato/FaleConosco";
+$route['trabalhe-conosco'] = "Contato/TrabalheConosco";
--- site-institucional/site-institucional/application/controllers/Institucional.php
+++ site-institucional/site-institucional/application/controllers/Institucional.php
@@ -3,6 +3,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Institucional extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->output->cache(1440);
+	}
+	
 	public function index()
 	{
 		$data['title'] = "LCI | Home";
